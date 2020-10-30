042fbaeba933ead4f5831cc8bd01d05cca06d4a0
Dylan-Carriere <dylan31120@gmail.com>
2020-10-30 10:50:56 +0100

    first commit

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+import * as mapboxgl from 'mapbox-gl';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lat = 43.63085694273947;
+  lng = 1.3628196716308594;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+      this.map = new mapboxgl.Map({
+        accessToken: environment.mapbox.accessToken,
+        container: 'map',
+        style: this.style,
+        zoom: 13,
+        center: [this.lng, this.lat]
+    });
+    // Add map controls
+    this.map.addControl(new mapboxgl.NavigationControl());
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiZHlsYW5tYXAiLCJhIjoiY2tncWQyano4MGtpNDJ0bmxlbGZ4anBvNCJ9.ONq48avAM2h_f3iorQuuOQ'
+  }
 };
 
 /*
