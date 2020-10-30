fc82f6991c66758e7a25867eebb89b9d1c35a1e0
Dylan-Carriere <dylan31120@gmail.com>
2020-10-30 15:16:47 +0100

    add footer and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
